b4d16b1ea2b5c7889648967a6a64bdfc60e5ad90
Elvira Zhekey <64586603+elkazh@users.noreply.github.com>
2023-12-02 20:39:38 +0600

    Add files via upload

---
+++ Variables.java
@@ -0,0 +1,22 @@
+package org.example;
+
+public class Variables {
+    public static void main(String[] args) {
+        int myInt = 558;
+        short myShort = 3266;
+        long myLong = 28993555;
+
+        double myDouble = 235.36;
+        float myFloat = 6348.45f;
+
+        char myChar = 'h';
+        boolean myBoolean = false;
+
+        byte myByte = -100;
+
+        System.out.println(myInt + ", "+ myShort + ", "+ myLong);
+        System.out.println(myDouble + ", "+ myFloat);
+        System.out.println(myChar + ", " + myBoolean);
+        System.out.println(myByte);
+    }
+}
