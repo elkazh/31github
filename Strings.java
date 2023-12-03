5362084aed04252b0e28b649e602a6dc52c6ce00
Elvira Zhekey <64586603+elkazh@users.noreply.github.com>
2023-12-03 20:06:51 +0600

    Add files via upload

---
+++ Strings.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Strings {
+    public static void main(Strings[] args) {
+        int myInt = 5;
+        String myString = "Hello";
+        String space = " ";
+        String name = "Elvira";
+        System.out.println(myString+space+name);
+        System.out.println("Hello"+ " "+ "John");
+        System.out.println("My number is "+myString);
+    }
+}
